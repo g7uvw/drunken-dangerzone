7052819d8f162075151c88f6eb04e8a0de11fb26
David <dave@webshed.org>
2015-03-29 22:16:13 +0100

    Towards a working build...

--- rollingroad/rollingroad.ino
+++ rollingroad/rollingroad.ino
@@ -1,21 +1,36 @@
 // Rolling Road 
-// (c) David Mills 2014
+// (c) David Mills 2015
 
 #include <FreqMeasure.h>
-int potval = 0, oldpotvol = 0;
-double sum=0;
-int count=0;
-float previousfrequency = 0;
+
+// Pin mappings 
+const int LEDpin = 13;
+const int BRAKEpin = 23;
+const int READYpin = 22;
+const int POTPpin = 0;  // Analog 0 (PIN 14 on Teensy 3.1)
+
+// Braking POT variables
+int potval = 0, oldpotval = 0;
+const int numReadings = 20;
+int POTreadings[numReadings];      // the readings from the analog input
+int POTindex = 0;                  // the index of the current reading
+int POTtotal = 0;                  // the running total
+int POTaverage = 0;                // the average
+
+
+float basefrequency = 0;
 float currentfrequency = 0;
-const int ledPin = 13;
-const int brakepin = 23;
+
+// Function protoypes
+int getPOTaverage(void);
+int getFREQaverage(void);
+
 void setup()
 {
- 
  analogWriteResolution(10);          // 10 bit PWM resolution (0 - 1023)
  analogWriteFrequency(23,46875);     // pin 23 as output, PWM frequency 46875Hz
  FreqMeasure.begin();                // start measuring the speed pulses
- pinMode(ledPin, OUTPUT);
+ pinMode(LEDpin, OUTPUT);  
 }
 
 void loop()
@@ -26,20 +41,32 @@ void loop()
  // At this point it's up to this code to keep the road at this speed by providing more
  // braking as the car tries to speed up.
  
- //read the pot
- potval = analogRead(0);             // analog 0, pin 14 on teensy 3.1 
+ // read the pot & wait for it to become stable (value doesn't change by more than +/- 2)
+ // apply braking too, as that's what the driver will detected when setting the pot
+
+while(!(unsigned) (POTaverage-(POTaverage-2)) <= ((POTaverage+2)-(POTaverage-2)))
+{
+ POTaverage = getPOTaverage();
+ analogWrite(BRAKEpin,POTaverage);
+}
+
+// when we make it here, the driver should have finished setting pot for his choice of speed
+// get a baseline frequency (speed)
+
+basefrequency = getFREQaverage(50);  // take 50 samples
+digitalWriteFast(READYpin, HIGH);    // set the READY LED on
  
- if (potval < oldpotval)             // reducing load from road, prior to speeding up?
- {
-    if (FreqMeasure.available())
-    {
-      //invalidate old frequency 
-      previousfrequency = FreqMeasure.countToFrequency(FreqMeasure.read());
-    }
- }
+// if (potval < oldpotval)             // reducing load from road, prior to speeding up?
+// {
+//    if (FreqMeasure.available())
+//    {
+//      //invalidate old frequency 
+//      previousfrequency = FreqMeasure.countToFrequency(FreqMeasure.read());
+//    }
+// }
  
  oldpotval = potval;                // save the current pot value for checking next loop around
- digitalWriteFast(ledPin, LOW);     // set the LED off
+ digitalWriteFast(LEDpin, LOW);     // set the LED off
 
  
  if (FreqMeasure.available())
@@ -49,15 +76,66 @@ void loop()
     if (count > 2)
       {
         currentfrequency = FreqMeasure.countToFrequency(sum / count);
-        if (currentfrequency > previousfrequency)    // we're still speeding up
+        if (currentfrequency > previousfrequency)    // faster than we should be
           {
-            digitalWriteFast(ledPin, HIGH);   // set the LED on
+            digitalWriteFast(LEDpin, HIGH);   // set the LED on
             // going faster this time around, lengthen the brake pulses.
-            analogWrite(brakepin,(potval*2)); 
+            analogWrite(BRAKEpin,(potval*2)); 
           }
-         previousfrequency = currentfrequency;
+         
        }
    }
  else
-   analogWrite(brakepin,potval);
+   analogWrite(BRAKEpin,potval);
+   previousfrequency = currentfrequency;
+}
+
+
+
+int getPOTaverage(void)
+{
+ // zero out the POT array
+  for (unsigned int i = 0; i < numReadings; i++)
+   POTreadings[i] = 0;  
+    
+  for(unsigned int i = 0; i< numReadings ; i++)
+  {
+    POTtotal= POTtotal - POTreadings[POTindex];        
+    // read from the sensor:  
+    POTreadings[POTindex] = analogRead(POTpin);
+    // add the reading to the total:
+    POTtotal= POTtotal + POTreadings[POTindex];      
+    // advance to the next position in the array:  
+    POTindex++;                    
+
+    // if we're at the end of the array...
+    if (POTindex >= numReadings)              
+      // ...wrap around to the beginning:
+      POTindex = 0;                          
+  }
+  
+  // calculate the average:
+  int average = POTtotal / numReadings; 
+  return average;
+}
+
+
+int getFREQaverage(int samples)
+{
+  double sum=0;
+  int count=0;
+  
+  for (count = 0; count < samples; count++)
+  {
+    if (FreqMeasure.available())
+     {
+      sum += FreqMeasure.read();
+     }
+     else
+       count--;  // no reading to be had, roll back in loop and try again
+     delay(10);  // small delay to help things along
+  }
+
+    int average = FreqMeasure.countToFrequency(sum / samples);
+    return average;
 }
