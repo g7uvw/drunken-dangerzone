1eab9cfc95e54e14e4be564a594fbe5ae52e6710
David <dave@webshed.org>
2015-11-22 20:46:28 +0000

    Updates to libraries

---
+++ libraries/st7783/examples/touchscreendemo/touchscreendemo.ino
@@ -0,0 +1,66 @@
+// Touch screen library with X Y and Z (pressure) readings as well
+// as oversampling to avoid 'bouncing'
+// This demo code returns raw readings, public domain
+
+#include <stdint.h>
+#include "TouchScreen.h"
+#include "TFTLCD.h" // not used, but needed for board setup
+
+#define YP A1  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 7   // can be a digital pin
+#define XP 6   // can be a digital pin
+
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// For the one we're using, its 300 ohms across the X plate
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+
+
+#define LCD_CS A3
+#define LCD_CD A2
+#define LCD_WR A1
+#define LCD_RD A0 
+// optional
+#define LCD_RESET A4
+
+// Color definitions
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0 
+#define WHITE           0xFFFF
+
+TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("Touchscreen demo");
+  pinMode(13, OUTPUT);
+}
+
+void loop(void) {
+  // a point object holds x y and z coordinates
+  digitalWrite(13, HIGH);
+  Point p = ts.getPoint();
+  digitalWrite(13, LOW);
+
+  pinMode(XM, OUTPUT);
+  pinMode(YP, OUTPUT);
+
+  // we have some minimum pressure we consider 'valid'
+  // pressure of 0 means no pressing!
+  if (p.z > ts.pressureThreshhold) {
+     Serial.print("X = "); Serial.print(p.x);
+     Serial.print("\tY = "); Serial.print(p.y);
+     Serial.print("\tPressure = "); Serial.println(p.z);
+  }
+
+  delay(100);
+}
+
